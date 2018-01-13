4b3d8978bf6f50d58b08fafa73a984f029ab8e00
RosyG <rmga2106@gmail.com>
2018-01-12 18:46:31 -0600

    añadiendo menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // La funcionalidad de tu proyecto
+/*
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
+*/
+$(".button-collapse").sideNav();
